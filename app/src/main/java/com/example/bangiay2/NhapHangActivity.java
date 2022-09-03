3e43c9f17dbfe9a680a95a8f3c75697cd60b4752
HuyVuong <vuonghuy12345@gmail.com>
2022-09-03 11:20:02 +0700

    XongNhapHang

---
+++ app/src/main/java/com/example/bangiay2/NhapHangActivity.java
@@ -0,0 +1,14 @@
+package com.example.bangiay2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NhapHangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nhap_hang);
+    }
+}
